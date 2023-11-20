f80b85c8d329e2bd491850f2ff51d9335f234735
fauzanansyori122 <142910263+fauzanansyori122@users.noreply.github.com>
2023-11-20 10:24:00 +0700

    Add files via upload

--- admin/index.php
+++ admin/index.php
@@ -1,157 +1,169 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <meta name="description" content="" />
-        <meta name="author" content="" />
-        <title>Vendora-Store Admin</title>
-        <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
-        <link href="css/styles.css" rel="stylesheet" />
-        <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
-    </head>
-    <body class="sb-nav-fixed">
-        <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
-            <!-- Navbar Brand-->
-            <a class="navbar-brand ps-3" href="index.php">Admin</a>
-            <!-- Sidebar Toggle-->
-            <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
-            <!-- Navbar Search-->
-            <form class="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
-            </form>
-            <!-- Navbar-->
-            <ul class="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
-                    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                        <li><a class="dropdown-item" href="../user/index.php">Home</a></li>
-                        <li><a class="dropdown-item" href="../user/about.php">Profil</a></li>
-                        <li><hr class="dropdown-divider" /></li>
-                        <li><a class="dropdown-item" href="https://google.com">Logout</a></li>
-                    </ul>
-                </li>
-            </ul>
-        </nav>
-        <div id="layoutSidenav">
-            <div id="layoutSidenav_nav">
-                <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
-                    <div class="sb-sidenav-menu">
-                        <div class="nav">
-                            <div class="sb-sidenav-menu-heading">Core</div>
-                            <a class="nav-link" href="index.php">
-                                <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
-                                Dashboard
-                            </a>
-                        </div>
+
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>Vendora-Store Admin</title>
+    <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
+    <link href="css/styles.css" rel="stylesheet" />
+    <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+</head>
+
+<body class="sb-nav-fixed">
+    <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
+        <!-- Navbar Brand-->
+        <a class="navbar-brand ps-3" href="index.php">Admin</a>
+        <!-- Sidebar Toggle-->
+        <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i
+                class="fas fa-bars"></i></button>
+        <!-- Navbar Search-->
+        <form class="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
+        </form>
+        <!-- Navbar-->
+        <ul class="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown"
+                    aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
+                <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                    <li><a class="dropdown-item" href="../user/index.php">Home</a></li>
+                    <li><a class="dropdown-item" href="../user/about.php">Profil</a></li>
+                    <li>
+                        <hr class="dropdown-divider" />
+                    </li>
+                    <li><a class="dropdown-item" href="https://google.com">Logout</a></li>
+                </ul>
+            </li>
+        </ul>
+    </nav>
+    <div id="layoutSidenav">
+        <div id="layoutSidenav_nav">
+            <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+                <div class="sb-sidenav-menu">
+                    <div class="nav">
+                        <div class="sb-sidenav-menu-heading">Core</div>
+                        <a class="nav-link" href="index.php">
+                            <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                            Dashboard
+                        </a>
                     </div>
-                </nav>
-            </div>
-            <div id="layoutSidenav_content">
-                <main>
-                    <div class="container-fluid px-4">
-<?php
-                include '../koneksi.php';
-                $query = mysqli_query($conn, "SELECT * from produk as p join size_produk as up on p.id_size = up.id_size join status_produk as sp on p.id_status = sp.id_status join brand_produk as gp on p.id_brand = gp.id_brand ORDER BY p.id ASC;");
-                ?>
-                        <h1 class="mt-4">Vendora</h1>
-                        <ol class="breadcrumb mb-4">
-                            <li class="breadcrumb-item active"><a class="btn btn-info" href="tambah.php?nama_halaman=Menambah Sepatu">
-                    Menambah Sepatu </a></li>
-                        </ol>
-                        <div class="card mb-4">
-                            <div class="card-header">
-                                <i class="fas fa-table me-1"></i>
-                                List Data Sepatu
-                            </div>
-                            <div class="card-body">
-                                <table id="datatablesSimple">
-                                    <thead>
-                        <tr>
-                            <th>
-                                Id
-                            </th>
-                            <th>
-                                Nama
-                            </th>
-                            <th>
-                                Gambar
-                            </th>
-                            <th>
-                                Harga
-                            </th>
-                            <th>
-                                Brand
-                            </th>
-                            <th>
-                                Size
-                            </th>
-                            <th>
-                                Status
-                            </th>
-                            <th>
-                                Option
-                            </th>
-                        </tr>
-                    </thead>
-                    <?php
-                    if (mysqli_num_rows($query) > 0) {
-                        while ($data = mysqli_fetch_array($query)) {
-                            ?>
-                            <tr>
-                                <td>
-                                    <?php echo $data["id"] ?>
-                                </td>
-                                <td>
-                                    <?php echo $data["nama_produk"] ?>
-                                </td>
-                                <td> <img src="<?php echo $data["image"] ?>" width="100"> </td>
-                                <td>
-                                    <?php echo $data["harga"] ?>
-                                </td>
-                                <td>
-                                    <?php echo $data["brand"] ?>
-                                </td>
-                                <td>
-                                    <?php echo $data["size"] ?>
-                                </td>
-                                <td>
-                                    <?php echo $data["status_produk"] ?>
-                                </td>
-                                <td>
-                                    <a href="edit.php?id=<?php echo $data["id"] ?>" class="badge text-bg-warning "> Ubah </a>&nbsp;
-                                    <a href="proses_hapus.php?id=<?php echo $data["id"] ?>" class="badge text-bg-danger">Delete
-                                    </a>
-                                </td>
-                            </tr>
-                        <?php } ?>
-                    <?php } ?>
-                                    </tbody>
-                                </table>
-                            </div>
-                        </div>
-                    </div>
-                </main>
-                <footer class="py-4 bg-light mt-auto">
-                    <div class="container-fluid px-4">
-                        <div class="d-flex align-items-center justify-content-between small">
-                            <div class="text-muted">Vendora &copy; Your Website 2023</div>
-                            <div>
-                                <a href="#">Privacy Policy</a>
-                                &middot;
-                                <a href="#">Terms &amp; Conditions</a>
-                            </div>
-                        </div>
-                    </div>
-                </footer>
-            </div>
+                </div>
+            </nav>
         </div>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-        <script src="js/scripts.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
-        <script src="assets/demo/chart-area-demo.js"></script>
-        <script src="assets/demo/chart-bar-demo.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/umd/simple-datatables.min.js" crossorigin="anonymous"></script>
-        <script src="js/datatables-simple-demo.js"></script>
-    </body>
+        <div id="layoutSidenav_content">
+            <main>
+                <div class="container-fluid px-4">
+                    <?php
+                    include '../koneksi.php';
+                    $query = mysqli_query($conn, "SELECT * from produk as p join size_produk as up on p.id_size = up.id_size join status_produk as sp on p.id_status = sp.id_status join brand_produk as gp on p.id_brand = gp.id_brand ORDER BY p.id ASC;");
+                    ?>
+                    <h1 class="mt-4">Vendora</h1>
+                    <ol class="breadcrumb mb-4">
+                        <li class="breadcrumb-item active"><a class="btn btn-info"
+                                href="tambah.php?nama_halaman=Menambah Sepatu">
+                                Menambah Sepatu </a></li>
+                    </ol>
+                    <div class="card mb-4">
+                        <div class="card-header">
+                            <i class="fas fa-table me-1"></i>
+                            List Data Sepatu
+                        </div>
+                        <div class="card-body">
+                            <table id="datatablesSimple">
+                                <thead>
+                                    <tr>
+                                        <th>
+                                            Id
+                                        </th>
+                                        <th>
+                                            Nama
+                                        </th>
+                                        <th>
+                                            Gambar
+                                        </th>
+                                        <th>
+                                            Harga
+                                        </th>
+                                        <th>
+                                            Brand
+                                        </th>
+                                        <th>
+                                            Size
+                                        </th>
+                                        <th>
+                                            Status
+                                        </th>
+                                        <th>
+                                            Option
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <?php
+                                if (mysqli_num_rows($query) > 0) {
+                                    while ($data = mysqli_fetch_array($query)) {
+                                        ?>
+                                        <tr>
+                                            <td>
+                                                <?php echo $data["id"] ?>
+                                            </td>
+                                            <td>
+                                                <?php echo $data["nama_produk"] ?>
+                                            </td>
+                                            <td> <img src="<?php echo $data["image"] ?>" width="100"> </td>
+                                            <td>
+                                                <?php echo $data["harga"] ?>
+                                            </td>
+                                            <td>
+                                                <?php echo $data["brand"] ?>
+                                            </td>
+                                            <td>
+                                                <?php echo $data["size"] ?>
+                                            </td>
+                                            <td>
+                                                <?php echo $data["status_produk"] ?>
+                                            </td>
+                                            <td>
+                                                <a href="edit.php?id=<?php echo $data["id"] ?>" class="badge text-bg-warning ">
+                                                    Ubah </a>&nbsp;
+                                                <a href="proses_hapus.php?id=<?php echo $data["id"] ?>"
+                                                    class="badge text-bg-danger">Delete
+                                                </a>
+                                            </td>
+                                        </tr>
+                                    <?php } ?>
+                                <?php } ?>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </main>
+            <footer class="py-4 bg-light mt-auto">
+                <div class="container-fluid px-4">
+                    <div class="d-flex align-items-center justify-content-between small">
+                        <div class="text-muted">Vendora &copy; Your Website 2023</div>
+                        <div>
+                            <a href="#">Privacy Policy</a>
+                            &middot;
+                            <a href="#">Terms &amp; Conditions</a>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+        crossorigin="anonymous"></script>
+    <script src="js/scripts.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
+    <script src="assets/demo/chart-area-demo.js"></script>
+    <script src="assets/demo/chart-bar-demo.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/umd/simple-datatables.min.js"
+        crossorigin="anonymous"></script>
+    <script src="js/datatables-simple-demo.js"></script>
+</body>
+
 </html>
